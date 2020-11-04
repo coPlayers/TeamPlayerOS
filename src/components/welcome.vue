b236148bf5a94e6070dfd0e7bba5c604e45f52e6
stefian <v_ianta@yahoo.com>
2020-11-04 15:42:32 -0400

    + <p>serve monetize repeat 24/7 - 1st gen. DNA OS</p>

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="welcome">
     <h1>Retail @ AI Speed</h1>
-    <p>develop monetize repeat 24/7 - 1st gen DNA OS</p>
+    <p>serve monetize repeat 24/7 - 1st gen. DNA OS</p>
     <div class="cta">
       <router-link to="/signup">Sign Up</router-link>
       <router-link to="/signin">Sign In</router-link>
